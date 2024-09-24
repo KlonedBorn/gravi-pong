7d9129805bc0671c76019f0cf0aff6f652cbe362
kingineer <k.michael.king@outlook.com>
2024-09-23 23:49:53 -0400

    + Changed script

---
+++ Activate.ps1
@@ -0,0 +1,3 @@
+git config --global user.name "kingineer"
+git config --global user.email "k.michael.king@outlook.com"
+git config --global credential.helper wincred
--- Git.ps1
+++
@@ -1,3 +0,0 @@
-git config --global --unset user.name
-git config --global --unset user.email
-git config --global --unset credential.helper
